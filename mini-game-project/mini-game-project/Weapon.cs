05a68400e799401aa935cc5f2e566adabe4ae9f3
1068447 <1068447@hr.nl>
2024-02-19 20:30:31 +0100

    weapon methods

--- mini-game-project/mini-game-project/Weapon.cs
+++ mini-game-project/mini-game-project/Weapon.cs
@@ -3,12 +3,29 @@
     public int Id;
     public string Name;
     public int Damage;
+    public bool Equipped;
 
     public Weapon(int id, string name, int damage)
     {
         Id = id;
         Name = name;
         Damage = damage;
+        Equipped = false; // Default to not equipped.
     }
 
+    public void Use()
+    {
+        Console.WriteLine($"You used {Name} and dealt {Damage} damage!");
+    }
+
+    public void Drop()
+    {
+        Console.WriteLine($"You dropped {Name}.");
+    }
+
+    public void Equip()
+    {
+        Equipped = true;
+        Console.WriteLine($"You equipped {Name}.");
+    }
 }
