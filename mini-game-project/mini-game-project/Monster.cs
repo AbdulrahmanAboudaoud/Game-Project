de37524d2518f7c78603bf62834ce3cc1ba16161
AbdulrahmanAboudaoud <70692341+AbdulrahmanAboudaoud@users.noreply.github.com>
2024-02-19 20:30:53 +0100

    Debug
    Used systems added and class monster fixed.

--- mini-game-project/mini-game-project/Monster.cs
+++ mini-game-project/mini-game-project/Monster.cs
@@ -8,14 +8,20 @@ namespace mini_game_project
 {
     internal class Monster
     {
+        public string Name;
+        public int ID;
+        public int CurrentHitPoints;
+        public int MaximumDamagepoints;
+        public int MaximunHitPoints;
 
-        public monster(int id, string name, int CurrentHitPoints, int MaximumDamagepoints, int MaximunHitPoints);
+        public Monster(int id, string name, int currentHitPoints, int maximumDamagepoints, int maximunHitPoints)
+        {
+            ID = id;
+            Name = name;
+            CurrentHitPoints = currentHitPoints;
+            MaximumDamagepoints = maximumDamagepoints;
+            MaximunHitPoints = maximunHitPoints;
 
-        Id =  int id;
-        name = string name;
-
-
-
-
-
+        }
     }
+}
